591ae1ff2a5ecb252bd812b9b7ec07ae56dfa20c
Juan C. Poos <94860562+JuanCPoos@users.noreply.github.com>
2022-11-22 17:34:15 -0300

    First Commit

---
+++ Digital Clock/index.js
@@ -0,0 +1,37 @@
+/* instanciamos los elementos en html */
+const hourElem = document.getElementById("hour");
+const minutesElem = document.getElementById("minutes");
+const secondElem = document.getElementById("seconds");
+const ampmElem = document.getElementById("ampm");   
+const paisesElem = document.getElementById("selectPaises");
+
+/* obtenemos los datos de la computadora, usando el constructor de fechas */
+function updateClock() { 
+    let hour = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    let ampm = "AM" /* inicializamos ampm = AM */
+    let paises;  
+
+    if (hour > 12) {
+        hour = hour - 12
+        ampm = "PM"
+    }
+
+    /* Si hour es menor a 10, agregar "0" mas la hora
+    de los contrario la hora como esta*/
+    hour = hour < 10 ? "0" + hour : hour;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    hourElem.innerText = hour;
+    minutesElem.innerText = minutes;
+    secondElem.innerText = seconds;
+    ampmElem.innerText = ampm;
+    
+    setTimeout(() => {
+        updateClock()
+    }, 1000)
+}
+
+updateClock();
