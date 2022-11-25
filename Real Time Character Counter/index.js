edeb21ead750c03cb44be079985e354576cc94e2
Juan C. Poos <94860562+JuanCPoos@users.noreply.github.com>
2022-11-24 21:04:20 -0300

    Fin Commit

--- Real Time Character Counter/index.js
+++ Real Time Character Counter/index.js
@@ -1,1 +1,17 @@
-const txtEl = document.getElementById('textarea');
+const txtAreaEl = document.getElementById('textarea');
+const totalCounterEl = document.getElementById('total-counter');
+const remaiCounterEl = document.getElementById('remaining-counter');
+
+/* tomar la acción realizada en textarea y llamar a una funcion.
+metodo KEY UP: cuando escribimos */
+txtAreaEl.addEventListener('keyup', () => {
+    updateCounter();    
+});
+
+updateCounter();
+
+function updateCounter() { 
+    totalCounterEl.innerText = txtAreaEl.value.length;
+    remaiCounterEl.innerText = txtAreaEl.getAttribute('maxlength') - txtAreaEl.value.length;
+}
+
